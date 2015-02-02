94f0c761e26864257aab035b5e0cbb0950a10706
Samuel Parkinson <sam@graze.com>
2015-02-02 15:40:21 +0000

    Add copyright notices to source files.

--- src/FeedInterface.php
+++ src/FeedInterface.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of graze/feed.
+ *
+ * Copyright (c) 2015 Nature Delivered Ltd.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/graze/feed/blob/master/LICENSE
+ */
+
 namespace Graze\Feed;
 
 interface FeedInterface
